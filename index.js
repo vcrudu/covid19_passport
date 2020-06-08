bdb9568635f8950b41cd43850af06f15229d8839
victor.crudu+1-at-160466482332 <vcrudu@hotmail.com>
2020-06-08 18:15:17 +0100

    Fixing the logkitty security issue

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
  *   All rights reserved.
  */
 
+import 'react-native-gesture-handler'
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
 import {AppRegistry} from 'react-native';
 import App from './src/components/App';
+import Home from './src/components/Home';
 import Login from './src/components/Login';
+import SignUp from './src/components/SignUp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => App);
